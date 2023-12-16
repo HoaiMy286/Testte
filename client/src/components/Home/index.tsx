c6607111ce2fecae1bf57b84a84c82ad84a6769a
HoaiMy286 <my.nguyenmy@hcmut.edu.vn>
2023-12-16 23:39:16 +0700

    first commit

---
+++ client/src/components/Home/index.tsx
@@ -0,0 +1,60 @@
+
+import MiniNoti from './MiniNoti'
+import Weather from './Weather'
+import State from './State'
+import GrownPlant from './Grownplant'
+import Start from './Start'
+import '../../scss/homepage.scss'
+import { Typography } from '@mui/material'
+import { Container } from '@mui/system'
+import React, { memo, useEffect, useState } from 'react'
+import '../../scss/homepage.scss'
+import { adafruitAxios } from '../../api'
+import adafruitService from '../../service/adafruitService'
+
+const HomePage = () => {
+
+
+    const [noti_led, setNotiled] = useState<String>()
+    const [noti_pump, setNotipump] = useState<String>()
+    const [temperature, setTemperature] = useState<String>()
+    const [lightIntensity, setLightIntensity] = useState<String>()
+    const [landMoisture, setLandMoisture] = useState<String>()
+    const [humidity, sethumidity] = useState<String>()
+    const [start, setStart] = useState<String>()
+
+    useEffect(() => {
+
+        adafruitService.noti_led(setNotiled)
+        adafruitService.noti_pump(setNotipump)
+        adafruitService.temperature(setTemperature)
+        adafruitService.lightIntensity(setLightIntensity)
+        adafruitService.landMoisture(setLandMoisture)
+        adafruitService.humidity(sethumidity)
+        adafruitService.start_state(setStart)
+
+    }, [temperature, lightIntensity, landMoisture, humidity,start])
+
+  
+
+
+
+
+    return (
+        <div className="container" style={{ display: 'flex' }}>
+            <div className='home-left' >
+                <Weather />
+               
+                    <MiniNoti />
+             
+            </div>
+            <div className='home-right'>
+                <Start/>
+                <GrownPlant />
+                <State />
+
+            </div>
+        </div>
+    )
+}
+export default HomePage
